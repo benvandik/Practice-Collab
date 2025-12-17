735114308a02fdc52127c358569f065b52d80a3d
M-Ronaldo07 <mustafasyedpsn@gmail.com>
2025-12-17 12:41:04 -0500

    print umer

--- MainView.java
+++ MainView.java
@@ -3,5 +3,6 @@ class MainView {
         System.out.println("Whats up gng ");
         System.out.print("bruh ");
         System.out.println("save ");
+        System.out.print("Omer troll bruh ");
     }
 }
