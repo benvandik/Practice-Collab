e633938bb5b2965fe581b03e0c148eb53a92f8c0
benvandik <chahineomer7@gmail.com>
2025-12-17 12:59:15 -0500

    chalactelistics

--- logic.java
+++ logic.java
@@ -1,6 +1,8 @@
+import javafx.layout.HBox
 public class logic {
     logic(){
 
+        Hbox bopx = new HBox ();
         System.out.println("hello");
         System.out.println("helloa");
         System.out.println("hallo");
