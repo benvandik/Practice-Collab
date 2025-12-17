25d62ad8af4ee82d56c87727bcae3fee7ed47959
M-Ronaldo07 <mustafasyedpsn@gmail.com>
2025-12-17 12:57:50 -0500

    Update logic.java

--- logic.java
+++ logic.java
@@ -1,6 +1,6 @@
 public class logic {
     logic(){
-
+        String title = "Bruh";
         System.out.println("hello");
         System.out.println("helloa");
         System.out.println("hallo");
